9aff25154ee81e882281fcc738faee21269eb960
NAD3R <naderdeghaili@gmail.com>
2026-01-12 18:59:30 +0100

    completed es2

--- src/es2/Main.java
+++ src/es2/Main.java
@@ -1,4 +1,27 @@
 package es2;
 
+import java.util.Scanner;
+
 public class Main {
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("inserisci i km percorsi");
+            int km = Integer.parseInt(sc.nextLine());
+
+            System.out.println("inserisci i litri consumati");
+            int l = Integer.parseInt(sc.nextLine());
+
+            int kmL = km / l;
+            System.out.println("la tua macchina fa " + kmL + " km/L");
+        } catch (ArithmeticException ex) {
+
+            System.out.println("errore hai diviso per 0");
+        } catch (NumberFormatException ex) {
+            System.out.println("digita un numero");
+        }
+    }
+
+
 }
