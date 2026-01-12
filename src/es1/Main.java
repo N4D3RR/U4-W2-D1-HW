43c940256abe0f5e2230673b4593ab7b5fd77d7d
NAD3R <naderdeghaili@gmail.com>
2026-01-12 16:56:54 +0100

    completed es1

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,4 +1,39 @@
 package es1;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //       int random = (int) (Math.random()*10);
+        Random random = new Random();
+
+        int[] arrayDiNumeri = new int[5];
+
+        for (int i = 0; i < arrayDiNumeri.length; i++) {
+            arrayDiNumeri[i] = random.nextInt(10) + 1; //+1 escludo lo 0 includo il 10
+
+        }
+        System.out.println(Arrays.toString(arrayDiNumeri));
+        int numero; //fuori dal do se no il while non riesce a leggerlo (consiglio di intelliJ)
+        do {
+            System.out.println("inserisci un numero da 1 a 10, 0 per terminare");
+            numero = Integer.parseInt(sc.nextLine());
+            if (numero != 0) {
+                try {
+                    System.out.println("scegli la posizione da 0 a 4");
+                    int posizione = Integer.parseInt(sc.nextLine());
+                    arrayDiNumeri[posizione] = numero;
+                    System.out.println(Arrays.toString(arrayDiNumeri));
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    System.out.println("scegli una posizione valida, da 0 a 4");
+
+                }
+            }
+
+        } while (numero != 0);
+
+    }
 }
